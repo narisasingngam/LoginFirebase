ad85d08e739631ccf5003dd359943d7bfb02a31a
narisasingngam <mmintttt@gmail.com>
2020-03-20 23:31:07 +0700

    add view post

--- app/src/main/java/com/example/loginfirebase/ViewPost.java
+++ app/src/main/java/com/example/loginfirebase/ViewPost.java
@@ -1,15 +1,20 @@
 package com.example.loginfirebase;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +29,56 @@ public class ViewPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_post);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference("User_Detail");
+
+        recyclerView = (RecyclerView) findViewById(R.id.viewPostData);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ViewPost.this));
+
+        Toast.makeText(ViewPost.this, "Please wait, it is loading ...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<ViewSingleItem, ShowDataViewHolder>(ViewSingleItem.class, R.layout.viewsingleitem, ShowDataViewHolder.class, myRef) {
+            @Override
+            protected void populateViewHolder(final ShowDataViewHolder showDataViewHolder, ViewSingleItem viewSingleItem, final int i) {
+                showDataViewHolder.Image_URL(viewSingleItem.getImage_URL());
+                showDataViewHolder.Image_Title(viewSingleItem.getImage_Title());
+
+                //Click at any row of the list and we will delete that row out of the database
+
+                showDataViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ViewPost.this);
+                        builder.setMessage("Delete?").setCancelable(false)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        int selectedItems = i;
+                                        mFirebaseAdapter.getRef(selectedItems).removeValue();
+                                        mFirebaseAdapter.notifyItemRemoved(selectedItems);
+                                        recyclerView.invalidate();
+                                        onStart();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                            });
+                        AlertDialog dialog = builder.create();
+                        dialog.setTitle("Are you sure?");
+                        dialog.show();
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(mFirebaseAdapter);
     }
 
 
